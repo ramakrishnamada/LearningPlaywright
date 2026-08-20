861ce5d0ef9d47ff3356b90f56ce25cb95276381
ramakrishnamada <ramakrishna.mada@gmail.com>
2026-08-20 18:24:13 +0530

    Add JS chapter practice files and reorganize chapter folders
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 01_chapter_Javascript/JSPractice.js
@@ -0,0 +1,3 @@
+console.log("Enteryour input");
+const data = require('fs').readFileSync(0, 'utf8');
+console.log("Hello", data);
---
+++ 02_chapter_javascript/JS_Practice.js/02_let_concept.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log(a);
+for(let a =0; a < 100; a++){
+    console.log(a);
+badcodeFunction();
+
+}
+
+function badcodeFunction(){
+    console.log("This is a bad code function");
+}
